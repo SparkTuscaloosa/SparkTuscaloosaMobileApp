381b9492fd8b7956d89620ab77b6cfdcdb2cf406
Wesley L Eldridge <weseldridge@gmail.com>
2018-10-17 15:34:14 -0500

    start of pulling in data

--- src/pages/surveys/surveys.ts
+++ src/pages/surveys/surveys.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SurveyPage } from '../survey/survey';
 
+import { Apollo, QueryRef } from 'apollo-angular';
+import {Subscription} from 'rxjs/Subscription';
+import { Observable } from 'rxjs';
 import gql from 'graphql-tag';
 
-const query = gql`
-  query Surveys {
+const SURVEY_QUERY = gql`
+  query surveys {
     surveys {
       id
       title
@@ -18,10 +21,38 @@ const query = gql`
   selector: 'page-surveys',
   templateUrl: 'surveys.html'
 })
-export class SurveysPage {
+export class SurveysPage implements OnInit, OnDestroy {
+  loading: boolean;
+  surveys: Observable<any>;
+  surveysRef: QueryRef<any>;
+  surveysSub: Subscription;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private apollo: Apollo) {}
+
+  ngOnInit() {
+
+    this.loading = true;
+    this.surveysRef = this.apollo.watchQuery({
+      query: SURVEY_QUERY,
+      fetchPolicy: 'network-only'
+    });
+
+    if(this.surveysSub) {
+      this.surveysSub.unsubscribe();
+    }
+
+    this.surveysSub = this.surveysRef
+      .valueChanges
+      .subscribe(({ loading, data }) => {
+        this.loading = loading;
+        this.surveys = data.surveys
+      })
   }
+
+  ngOnDestroy() {
+    this.surveysSub.unsubscribe();
+  }
+
   goToSurvey(params){
     if (!params) params = {};
     this.navCtrl.push(SurveyPage);
